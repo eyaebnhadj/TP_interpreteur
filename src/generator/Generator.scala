97af6ea38a7d9e6a59bb9de0d31ae87dbe20af05
othmane <othmaneouhaddou91@gmail.com>
2025-12-09 16:01:11 +0100

    piste verte done

--- src/generator/Generator.scala
+++ src/generator/Generator.scala
@@ -5,19 +5,26 @@ import Op.*
 import Ins.*
 
 type Code = List[Ins]
+
 object Generator:
   def gen (term: Term): Code = term match
-     case Number(n) => List(Ldi(n))
-     case BinaryExp(op, u, v) => 
-       val c_u = gen(u)
-       val c_v = gen(v)
-       c_u ::: (Push :: c_v) ::: List(gen_op(op))
-       
-     def gen_op(op: Op) = op match 
-       case Plus => Add
-       case Minus => Sub
-       case Times => Mul
-       case Divide => Div
-     
-       
-       
+  case Number(n) => List(Ldi(n))
+
+  case BinaryExp(op, u, v) =>
+  val c_u = gen(u)
+  val c_v = gen(v)
+  c_u ::: (Push :: c_v) ::: List(gen_op(op))
+
+  // Code MANQUANT à ajouter pour la conditionnelle
+  case IfZero(cond, z, nz) =>
+  gen(cond) ::: List(Test(gen(z), gen(nz)))
+
+  // Pour les autres cas non supportés par la VM piste verte (Var, Let, Fun...),
+  // vous pouvez laisser le compilateur lever une erreur ou mettre ???
+  case _ => throw new Exception("Construction non supportée par le générateur piste verte")
+
+  def gen_op(op: Op) = op match
+  case Plus => Add
+  case Minus => Sub
+  case Times => Mul
+  case Divide => Div
--- src/pcf/PCF.scala
+++ src/pcf/PCF.scala
@@ -1,40 +1,40 @@
 package pcf
 
-import evaluator.{EvaluationException, Evaluator}
-import evaluator.Evaluator.eval
-import typer.TypingException
-//import lexer.Lexer
-//import parser.Parser
-//import parser.AbstractParser
-import typer.Typer
-//import typer.TypingException
+import evaluator.Evaluator
+import typer.{Typer, Type}
 import parserANTLRv2.AbstractParser
+import ast.Term
+import generator.{Generator, Code} // Assurez-vous d'importer le générateur
 import java.io.{FileInputStream, InputStream}
 
 object PCF:
   def main(args: Array[String]): Unit =
-    val in: InputStream =
-      if args.isEmpty then
-        System.in
-      else
-        FileInputStream(args(0))
-//    Lexer(in)
-//    val exp = Parser.parse(Lexer.nextToken())
-//    val token = Lexer.nextToken()
-//    if token != lexer.Token.EOF then
-//      throw new Exception(s"Unexpected token $token after parsing complete expression $exp")
-//
-//    val term = AbstractParser.analyze(in)
-//    println(s"==> ${eval(term, Map())}")
-//    val typ = Typer.eval(term, Map())
-//    println(s"Type: $typ")
-//    val result = Evaluator.eval(term, Map())
-//    println(s"==> $result: $typ")
-    val term = AbstractParser.analyze(in)
-    val typ = Typer.eval(term, Map.empty)
-    println(s"Type: $typ")
-    val result = Evaluator.eval(term, Map.empty)
-    println(s"Result: $result :: $typ")
+  // Gestion du flux d'entrée (fichier ou entrée standard)
+  val in: InputStream =
+    if args.isEmpty || (args.length == 1 && args(0) == "-i") then
+      System.in
+    else
+  val filename = if args(0) == "-i" then args(1) else args(0)
+  FileInputStream(filename)
 
+  // Logique de choix entre interpréteur et compilateur
+  if args.contains("-i") then
+    println(s"==> ${interpret(in)}")
+  else
+    println(compile(in))
 
+  // Front-end : Analyse syntaxique et typage
+  def analyze(in: InputStream): (Term, Type) =
+  val term = AbstractParser.analyze(in)
+  (term, Typer.eval(term, Map()))
 
+  // Interpréteur : Combine front-end et évaluation
+  def interpret(in: InputStream): String =
+  val (term, typ) = analyze(in)
+  val value = Evaluator.eval(term, Map())
+  s"$value :: $typ"
+
+  // Compilateur : Combine front-end et génération de code
+  def compile(in: InputStream): Code =
+  val (term, _) = analyze(in)
+  Generator.gen(term)
